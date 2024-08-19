c7fb8f38df640599054002d5a2e0c219df7867f9
Chewbacca <higgimat000@warren.k12.in.us>
2024-08-19 13:40:59 -0400

    initial commit

--- src/barkingdog.java
+++ src/barkingdog.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class barkingdog {
+public class barkingdog {
+    public static void main(String[] args) {
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(true, -1));
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if(hourOfDay > 23 || hourOfDay < 0){
+            return false;
+        } else if (barking && (hourOfDay > 22 || hourOfDay < 8)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
